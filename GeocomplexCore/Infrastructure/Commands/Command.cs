d1b5eddcc7fa7b9ff3a4d66a7239b57ec89375c2
Александр Зайбель <alexzaibel98@mail.ru>
2022-08-03 16:38:19 +0700

    Добавил Команды

---
+++ GeocomplexCore/Infrastructure/Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GeocomplexCore.Infrastructure.Commands
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        // Функция которая возвращает истину/ложь 
+        public abstract bool CanExecute(object parameter);
+
+        // То что должно быть выполнено самой командой
+        public abstract void Execute(object parameter);
+    }
+}
