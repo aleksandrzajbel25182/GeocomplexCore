d5d14c66b6f9b0d19923e3e79a98a19ef328ceb7
Александр Зайбель <alexzaibel98@mail.ru>
2022-10-07 12:16:27 +0700

    Добавил новую View для "точек наблюдения"

---
+++ GeocomplexCore/Views/Pages/Polevoi/InfoPointObservationView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeocomplexCore.Views.Pages.Polevoi
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoPointObservationView.xaml
+    /// </summary>
+    public partial class InfoPointObservationView : Page
+    {
+        public InfoPointObservationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
