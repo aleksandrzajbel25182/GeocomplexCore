369067afa513dbdf7be2beb21dfff5125a3a63a1
Александр Зайбель <alexzaibel98@mail.ru>
2022-10-13 11:52:18 +0700

    Добавил комментарии

--- GeocomplexCore/Model/FormatCoordinatModel.cs
+++ GeocomplexCore/Model/FormatCoordinatModel.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace GeocomplexCore.Model
 {
+    /// <summary>
+    /// Класс формата координат
+    /// </summary>
     internal class FormatCoordinatModel
     {
         private string _name;
-     
+     /// <summary>
+     /// Имя формата
+     /// </summary>
         public string Name { get => _name; set => _name = value; }
 
        
