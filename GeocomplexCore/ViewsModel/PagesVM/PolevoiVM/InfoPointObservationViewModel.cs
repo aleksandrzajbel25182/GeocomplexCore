b67b289cce5557db283f0664a3b6f05219bf7ff8
Александр Зайбель <alexzaibel98@mail.ru>
2022-10-25 09:34:10 +0700

    Добавил реализацию выпадющего меню для координат, сделал конертацию, пока только на градусы минуты и секунды.

--- GeocomplexCore/ViewsModel/PagesVM/PolevoiVM/InfoPointObservationViewModel.cs
+++ GeocomplexCore/ViewsModel/PagesVM/PolevoiVM/InfoPointObservationViewModel.cs
@@ -2,13 +2,13 @@
 using Egor92.MvvmNavigation.Abstractions;
 using GeocomplexCore.BD;
 using GeocomplexCore.BD.Context;
+using GeocomplexCore.Model;
+using GeocomplexCore.Service;
 using GeocomplexCore.ViewsModel.Base;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
@@ -18,6 +18,8 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
         #region Параметры/Parametrs
 
         private NavigationManager navigationmaneger;
+        private ConverterCordinatsService converter;
+
 
         private string _Id;
         public string ID
@@ -47,32 +49,67 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
         private DateTime? _wDateStart;
         public DateTime? WDateStart
         {
-            get {
+            get
+            {
                 _wDateStart = new DateTime(Watchpoints.WpointDateAdd.Value.Year, Watchpoints.WpointDateAdd.Value.Month, Watchpoints.WpointDateAdd.Value.Day);
-                return _wDateStart; }
-            set =>Set(ref _wDateStart, value); 
+                return _wDateStart;
+            }
+            set => Set(ref _wDateStart, value);
         }
 
         private string _wnote;
 
         public string Wnote
         {
-            get {
+            get
+            {
                 _wnote = Watchpoints.WpointNote;
-                return _wnote; }
-            set =>Set(ref _wnote , value); 
+                return _wnote;
+            }
+            set => Set(ref _wnote, value);
         }
 
         private string _wlocation;
-
         public string Wlocation
         {
-            get {
+            get
+            {
                 _wlocation = Watchpoints.WpointLocation;
-                return _wlocation; }
-            set=>Set(ref _wlocation ,value);
+                return _wlocation;
+            }
+            set => Set(ref _wlocation, value);
         }
 
+        //долгота
+        private string[] _longitude;
+        public string[] Longitude
+        {
+            get { return _longitude; }
+            set => Set(ref _longitude, value);
+        }
+
+        //Широта
+        private string[] _width;
+        public string[] Width
+        {
+            get { return _width; }
+            set => Set(ref _width, value);
+
+        }
+
+        // Z точка или абс отметка
+        private double? _pointZ;
+        public double? PointZ
+        {
+            get {
+                foreach (var item in Watchpoints.WpointCoordinates)
+                {
+                    _pointZ = item.WpCoordinatesZ;
+                }
+              
+                return _pointZ; }
+            set { _pointZ = value; }
+        }
 
 
         private Watchpoint _watchpoints;
@@ -82,7 +119,9 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
             {
                 using (GeocomplexContext db = new GeocomplexContext())
                 {
-                    var data = db.Watchpoints.Where(w => w.WpointId == PassedParameter).ToList();
+                    var data = db.Watchpoints
+                        .Where(w => w.WpointId == PassedParameter)
+                        .Include(c => c.WpointCoordinates).ToList();
 
                     foreach (var item in data)
                     {
@@ -97,6 +136,8 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
                             WpointNote = item.WpointNote,
                             FUser = item.FUser,
                             WpointIndLandscape = item.WpointIndLandscape,
+                            WpointCoordinates = item.WpointCoordinates,
+
 
                         };
                     }
@@ -106,6 +147,27 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
                 }
             }
             set => Set(ref _watchpoints, value);
+        }
+
+        /// <summary>
+        /// Список формата
+        /// </summary>
+        private ObservableCollection<FormatCoordinatModel> _formatCoordinat;
+        public ObservableCollection<FormatCoordinatModel> FormatCoordinat { get => _formatCoordinat; set => Set(ref _formatCoordinat, value); }
+
+        /// <summary>
+        /// Выбранный формат
+        /// </summary>
+        private FormatCoordinatModel _formatordSelected;
+        public FormatCoordinatModel FormatordSelected
+        {
+            get { return _formatordSelected; }
+            set
+            {
+                _formatordSelected = value;
+                OnPropertyChanged("FormatordSelected");
+                FormatingCoord();
+            }
         }
 
 
@@ -142,6 +204,33 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
 
         #region Функции/Function
 
+        public void FormatingCoord()
+        {
+            switch (FormatordSelected.Name)
+            {
+                case "Десятичная":
+
+                    break;
+
+                case "Градусы,минуты,секунды":
+
+                    converter = new ConverterCordinatsService();
+                    string l = "", w = "";
+                    foreach (var item in Watchpoints.WpointCoordinates)
+                    {
+                        l = converter.FormatingDecimal(item.WpCoordinatesX);
+                        w = converter.FormatingDecimal(item.WpCoordinatesY);
+                    }
+
+                    l = l.Replace("°", "").Replace("'", "");
+                    w = w.Replace("°", "").Replace("'", "");
+                    Longitude = l.Split(new char[] { ' ' });
+                    Width = w.Split(new char[] { ' ' });
+
+                    break;
+
+            }
+        }
         #endregion
         // ---------------------------------------------------------------------------------------------------------------------
 
@@ -150,6 +239,12 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
         public InfoPointObservationViewModel(NavigationManager navigationmaneger)
         {
             this.navigationmaneger = navigationmaneger;
+
+            FormatCoordinat = new ObservableCollection<FormatCoordinatModel>()
+            {
+                new FormatCoordinatModel() { Name = "Десятичная" },
+                new FormatCoordinatModel() { Name = "Градусы,минуты,секунды" }
+            };
 
         }
     }
