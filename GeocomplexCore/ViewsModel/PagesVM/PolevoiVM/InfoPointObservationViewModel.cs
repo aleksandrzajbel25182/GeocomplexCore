0b82cd1f098d2fd3bb7512eb0ffad1445d52804a
Александр Зайбель <alexzaibel98@mail.ru>
2022-11-18 13:50:35 +0700

    добавил в выпадющие меню списки по : Форма рельефа, тип рельефа, подтип рельефа, высотность, экспозиция, крутизна склона, форма речной долины. Данные берут из базы и проверяются на наличе именно к определенной точки наблюдения.

--- GeocomplexCore/ViewsModel/PagesVM/PolevoiVM/InfoPointObservationViewModel.cs
+++ GeocomplexCore/ViewsModel/PagesVM/PolevoiVM/InfoPointObservationViewModel.cs
@@ -26,7 +26,7 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
         private NavigationManager navigationmaneger;
         private ConverterCordinatsService converter;
 
-        
+
         /// <summary>
         /// Маршрут по которому делалась ТН
         /// </summary>
@@ -115,7 +115,7 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
             set => Set(ref _width, value);
 
         }
-        
+
         //Кординыты X
         private double? _wpointX;
         public double? WpointX
@@ -151,13 +151,15 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
         private double? _pointZ;
         public double? PointZ
         {
-            get {
+            get
+            {
                 foreach (var item in Watchpoints.WpointCoordinates)
                 {
                     _pointZ = item.WpCoordinatesZ;
                 }
-              
-                return _pointZ; }
+
+                return _pointZ;
+            }
             set { _pointZ = value; }
         }
 
@@ -174,7 +176,7 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
                     var data = db.Watchpoints
                         .Where(w => w.WpointId == PassedParameter)
                         .Include(c => c.WpointCoordinates)
-                        .Include(r=>r.Route)
+                        .Include(r => r.Route)
                         .ToList();
 
                     foreach (var item in data)
@@ -191,7 +193,13 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
                             FUser = item.FUser,
                             WpointIndLandscape = item.WpointIndLandscape,
                             WpointCoordinates = item.WpointCoordinates,
-
+                            IdExposition = item.IdExposition,
+                            IdFormareliefa = item.IdFormareliefa,
+                            IdFormariver = item.IdFormariver,
+                            IdHeightreliefa = item.IdHeightreliefa,
+                            IdSlope = item.IdSlope,
+                            IdSubtypereliefa = item.IdSubtypereliefa,
+                            IdTypereliefa = item.IdTypereliefa
 
                         };
                     }
@@ -202,6 +210,237 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
             }
             set => Set(ref _watchpoints, value);
         }
+
+
+        #region Список Форма рельефа
+        private ObservableCollection<GuideFormareliefa> _formareliefa = new ObservableCollection<GuideFormareliefa>();
+        public ObservableCollection<GuideFormareliefa> Formareliefa
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideFormareliefas.ToList();
+                    foreach (var item in data)
+                    {
+                        _formareliefa.Add(new GuideFormareliefa
+                        {
+                            IdFormareliefa = item.IdFormareliefa,
+                            NameFormareliefa = item.NameFormareliefa
+                        });
+
+                    }
+                    for (int i = 0; i < _formareliefa.Count; i++)
+                    {
+                        if (_formareliefa[i].IdFormareliefa == Watchpoints.IdFormareliefa)
+                            SelectedFormarelirfa = _formareliefa[i];
+                    }
+
+
+                    return _formareliefa;
+                }
+            }
+            set { _formareliefa = value; }
+        }
+        public GuideFormareliefa SelectedFormarelirfa { get; set; }
+        #endregion
+
+        #region Cписок Тип рельефа 
+        private ObservableCollection<GuideTypereliefa> _typereliefa = new ObservableCollection<GuideTypereliefa>();
+        public ObservableCollection<GuideTypereliefa> Typereliefa
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideTypereliefas.ToList();
+                    foreach (var item in data)
+                    {
+                        _typereliefa.Add(new GuideTypereliefa
+                        {
+                            IdTypereliefa = item.IdTypereliefa,
+                            NameTypereliefa = item.NameTypereliefa
+                        });
+
+                    }
+                    for (int i = 0; i < _typereliefa.Count; i++)
+                    {
+                        if (_typereliefa[i].IdTypereliefa == Watchpoints.IdTypereliefa)
+                            SelectedTypereliefa = _typereliefa[i];
+                    }
+
+
+                    return _typereliefa;
+                }
+            }
+            set { _typereliefa = value; }
+        }
+        public GuideTypereliefa SelectedTypereliefa { get; set; }
+        #endregion
+
+        #region Список Подтип рельефа
+        private ObservableCollection<GuideSubtypereliefa> _sybtypereliefa = new ObservableCollection<GuideSubtypereliefa>();
+        public ObservableCollection<GuideSubtypereliefa> Sybtypereliefa
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideSubtypereliefas.ToList();
+                    foreach (var item in data)
+                    {
+                        _sybtypereliefa.Add(new GuideSubtypereliefa
+                        {
+                            IdSubtypereliefa = item.IdSubtypereliefa,
+                            NameSubtypereliefa = item.NameSubtypereliefa
+                        });
+
+                    }
+                    for (int i = 0; i < _sybtypereliefa.Count; i++)
+                    {
+                        if (_sybtypereliefa[i].IdSubtypereliefa == Watchpoints.IdSubtypereliefa)
+                            SelectedSybtypereliefa = _sybtypereliefa[i];
+                    }
+
+
+                    return _sybtypereliefa;
+                }
+            }
+            set { _sybtypereliefa = value; }
+        }
+        public GuideSubtypereliefa SelectedSybtypereliefa { get; set; }
+        #endregion
+
+        #region Список Высотность рельефа
+        private ObservableCollection<GuideHeightreliefa> _heightreliefa = new ObservableCollection<GuideHeightreliefa>();
+        public ObservableCollection<GuideHeightreliefa> Heightreliefa
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideHeightreliefas.ToList();
+                    foreach (var item in data)
+                    {
+                        _heightreliefa.Add(new GuideHeightreliefa
+                        {
+                            IdHeightreliefa = item.IdHeightreliefa,
+                            NameHeightreliefa = item.NameHeightreliefa
+                        });
+
+                    }
+                    for (int i = 0; i < _heightreliefa.Count; i++)
+                    {
+                        if (_heightreliefa[i].IdHeightreliefa == Watchpoints.IdHeightreliefa)
+                            SelectedHeightreliefa = _heightreliefa[i];
+                    }
+
+
+                    return _heightreliefa;
+                }
+            }
+            set { _ = value; }
+        }
+        public GuideHeightreliefa SelectedHeightreliefa { get; set; }
+        #endregion
+
+        #region Список Экспозиция
+        private ObservableCollection<GuideSprexposition> _exposition = new ObservableCollection<GuideSprexposition>();
+        public ObservableCollection<GuideSprexposition> Exposition
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideSprexpositions.ToList();
+                    foreach (var item in data)
+                    {
+                        _exposition.Add(new GuideSprexposition
+                        {
+                            IdSprexposition = item.IdSprexposition,
+                            NameSprexposition = item.NameSprexposition
+                        });
+
+                    }
+                    for (int i = 0; i < _exposition.Count; i++)
+                    {
+                        if (_exposition[i].IdSprexposition == Watchpoints.IdExposition)
+                            SelectedExposition = _exposition[i];
+                    }
+
+
+                    return _exposition;
+                }
+            }
+            set { _exposition = value; }
+        }
+        public GuideSprexposition SelectedExposition { get; set; }
+        #endregion
+
+        #region Список Крутизна склона
+        private ObservableCollection<GuideSlope> _slope = new ObservableCollection<GuideSlope>();
+        public ObservableCollection<GuideSlope> Slope
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideSlopes.ToList();
+                    foreach (var item in data)
+                    {
+                        _slope.Add(new GuideSlope
+                        {
+                            IdSlope = item.IdSlope,
+                            NameSlope = item.NameSlope
+                        });
+
+                    }
+                    for (int i = 0; i < _slope.Count; i++)
+                    {
+                        if (_slope[i].IdSlope == Watchpoints.IdSlope)
+                            SelectedSlope = _slope[i];
+                    }
+
+
+                    return _slope;
+                }
+            }
+            set { _slope = value; }
+        }
+        public GuideSlope SelectedSlope { get; set; }
+        #endregion
+
+        private ObservableCollection<GuideFormariver> _formariver = new ObservableCollection<GuideFormariver>();
+        public ObservableCollection<GuideFormariver> Formariver
+        {
+            get
+            {
+                using (GeocomplexContext db = new GeocomplexContext())
+                {
+                    var data = db.GuideFormarivers.ToList();
+                    foreach (var item in data)
+                    {
+                        _formariver.Add(new GuideFormariver
+                        {
+                            IdFormariver = item.IdFormariver,
+                            NameFormariver = item.NameFormariver
+                        });
+
+                    }
+                    for (int i = 0; i < _formariver.Count; i++)
+                    {
+                        if (_formariver[i].IdFormariver == Watchpoints.IdFormariver)
+                            SelectedFormariver = _formariver[i];
+                    }
+
+
+                    return _formariver;
+                }
+            }
+            set { _formariver = value; }
+        }
+        public GuideFormariver SelectedFormariver { get; set; }
+
 
         /// <summary>
         /// Список формата
@@ -261,10 +500,10 @@ namespace GeocomplexCore.ViewsModel.PagesVM.PolevoiVM
         private void OnBackNavigateCommandExcuted(object p)
         {
             LocatorStatic.Data.PageHeader = $"Участок";
-            this.navigationmaneger.Navigate("InfoDistrict",GlobalSet.NameDis);
+            this.navigationmaneger.Navigate("InfoDistrict", GlobalSet.NameDis);
         }
         #endregion
-   
+
         #endregion
         // ---------------------------------------------------------------------------------------------------------------------
 
